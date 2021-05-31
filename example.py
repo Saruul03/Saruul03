57c72ba5fa33d0daab8700deca9195ce0f83a752
Saruul Myagmar <saruul.m38@gmail.com>
2021-05-31 16:39:15 +0800

    search initial commit

---
+++ example.py
@@ -0,0 +1,10 @@
+
+# Curly braces is a list and list doesn't support mutability
+words = {"name": "Saruul", "age": 28, "Ethnicity": "XALX"}
+
+diction = ["sainuu", "yu", "baina", "daa"]
+
+diction[1] = "yutai"
+
+
+print(words["Ethnicity"])
